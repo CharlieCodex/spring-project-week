3ba3f8d58645b223235423f85b3dad5e7f84fcda
charliecodex <19782355+CharlieCodex@users.noreply.github.com>
2018-05-25 13:41:45 -0400

    Processed data with "with-mido/main.py" Added processed data folder to .gitignore

--- with-mido/main.py
+++ with-mido/main.py
@@ -1,48 +1,62 @@
-from mido import MidiFile, Message
+from mido import MidiFile, MidiTrack, Message, merge_tracks
 import numpy as np
 from os import listdir
-import reduce
 
 def parse_events(f):
     midi = MidiFile(f)
+    length = midi.length
     events = []
+    n = 0
     for msg in midi:
+        if msg.type == 'set_tempo':
+            print('\ttempo change to {} µspq after {} notes'.format(msg.tempo,n))
+            tempo = msg.tempo
+            n=0
         if msg.type == 'note_on' or msg.type == 'note_off':
+            n += 1
             events.append({
                 'type': msg.type,
-                'dt': msg.time,
+                'dt': msg.time/length,
                 'key': msg.note,
                 'vel': msg.velocity})
     return events
 
+def msg2vec(msg):
+    '''Transform a mido message into a vector of length 290'''
+    return event2vec({
+        'type': msg.type,
+        'dt': msg.time,
+        'key': msg.note,
+        'vel': msg.velocity})
+
+
 def event2vec(event):
     '''Transform a single event into a vector of length 290'''
-    dt = np.array([int(x) for x in '{:032b}'.format(int(event['dt']))]) # list of 0 and 1's
+    dt = np.array((event['dt'],)) # list of 0 and 1's
     on_off = np.array((
         0 if event['type'] == 'note_off' else 1,
         1 if event['type'] == 'note_off' else 0,)) # onehot encoding for on/off
     key = np.zeros((128,))
     key[event['key']] = 1
     vel = np.zeros((128,))
-    key[event['vel']] = 1
+    vel[event['vel']] = 1
+    # print('dt: ', event['dt'], dt)
     return np.concatenate((dt, on_off, key, vel))
 
 def vec2event(vec):
     '''Transform a vector of length 290 into a midi event'''
-    vec_dt = vec[:32] # get first 32 components [binary]
-    vec_on_off = vec[32:34] # get on/of components [1 hot]
-    vec_key = vec[34:34+128] # get key components [1 hot]
-    vec_vel = vec[34+128:] # get velocity components [1 hot]
-    str_dt = [str(e) for e in vec_dt] # convert first 32 components into strings
-    dt = int(''.join(str_dt),2) # join strings and cast as base 2 integer
-    on_off = 'note_off' if vec_on_off[0] else 'note_on' # extract on/off -ness of note
+    dt = vec[0]*500000 # get first component of vector
+    vec_on_off = vec[1:3] # get on/of components [1 hot]
+    vec_key = vec[3:3+128] # get key components [1 hot]
+    vec_vel = vec[3+128:] # get velocity components [1 hot]
+    on_off = 'note_off' if vec_on_off[0] == 0 else 'note_on' # extract on/off -ness of note
     key = np.argmax(vec_key) # get the 1-hot index
     vel = np.argmax(vec_vel) # get the 1-hot index
-    return {'type': on_off, 'dt': dt, 'key': key, 'vel': vel}
+    return {'type': on_off, 'dt': int(dt), 'key': key, 'vel': vel}
 
 def event2msg(event):
     '''Convert an internally used event dict into a mido midi message'''
-    return Message(event.type,
+    return Message(event['type'],
         time=event['dt'],
         note=event['key'],
         velocity=event['vel'])
@@ -58,3 +72,22 @@ def translate_dir(in_dir, out_dir):
     for in_path in listdir(in_dir):
         print('Processing {}'.format(in_path))
         translate_file(in_dir+in_path, out_dir+in_path.rstrip('.mid')+'.npy')
+
+if __name__ == '__main__':
+    # simple tests
+    translate_dir('../data/', '../processed/')
+
+def load_back():
+    vecs = np.load('test.npy')
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
+    for vec in vecs:
+        msg = event2msg(vec2event(vec))
+        #print(vec, msg)
+        track.append(msg)
+    track.append(Message('note_on', time=0, note=64, velocity=127))
+    track.append(Message('note_off', time=128, note=64, velocity=127))
+    track.append(Message('note_on', time=64, note=64, velocity=127))
+    track.append(Message('note_on', time=128, note=64, velocity=0))
+    mid.save('test.mid')
